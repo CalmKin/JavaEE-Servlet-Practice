98112e7009813aa2e17734edcee753d086667b62
calmkin <490363906@qq.com>
2022-12-22 19:54:03 +0800

    上传至GitHub

--- src/main/java/com/calmkin/service/PlaceService.java
+++ src/main/java/com/calmkin/service/PlaceService.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public interface PlaceService {
 //    查询所有地点
-    public PageBean<Place> selectByPage(int currPage,int pageSize);
+     PageBean<Place> selectByPage(int currPage,int pageSize);
 //    添加新的地点
-    public void addPlace(Place place);
+     void addPlace(Place place);
 //    修改地点信息
-    public void changeSingle(Place place);
+     void changeSingle(Place place);
 //    批量删除地点
-    public void deleteByIds(int[] ids);
+     void deleteByIds(int[] ids);
 
 //    获取所有可以使用的核酸检测点
-    public List<String> accessiblePlace();
+     List<String> accessiblePlace();
 }
